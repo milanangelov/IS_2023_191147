fc940b5a448ef9749e620ef66d08fb4e71ad3d4e
MilanAngelov <milan.angelov@students.finki.ukim.mk>
2023-07-10 23:52:23 +0200

    initial

---
+++ ECinema.Web/Models/DomainModels/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECinema.Web.Models.DomainModels
+{
+    public class Actor
+    {
+        public Guid Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public virtual ICollection<ActorsInMovies> ActorsInMovies { get; set; }
+    }
+}
